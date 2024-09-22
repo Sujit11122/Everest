8bef7a2c16aa02e7b9cb7e0dbc46830d707df49e
Sujit Gautam <gautamsujit341@gmail.com>
2024-09-21 22:16:52 -0500

    Formatted code

---
+++ app/components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return(
+        <div>
+            <footer className="text-center bg-orange-400" style={{border:"2px solid red"}}>
+            <p>Contact</p>
+            <p>Email: sujit.gautam@selu.edu</p>
+            <p>Phone number: 9859565676</p>
+          </footer>
+            </div>
+
+    )
+}
---
+++ app/components/navbar.tsx
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return(
+        <div>
+            <div className=" text-center font-bold text-green-600">
+        <h2> Project tailwind</h2>
+      </div>
+      <div className=".text-left text-black" >
+        <p> This is the left alignment</p>
+      </div>
+      <div className="text-right text-black">
+        <p> This is the right alignment</p>
+      </div>
+            </div>
+            )
+}
